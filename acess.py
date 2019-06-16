36f2360e4eb34a52b9eb3a7efca84352457a3235
Luis Otaño <luisencarnacion772@gmail.com>
2019-06-15 21:14:16 -0400

    "bien"

--- acess.py
+++ acess.py
@@ -7,7 +7,7 @@ H = input("desbloqueo of Shell: ")
 if H =="ningun systema es seguro":
     print("CORRECTO EMPEZANDO INSTALACION")
     time.sleep(5)
-    os.system("cd $HOME && git clone https://github.com/LX772/shell.git && bash shell/instalbl")
+    os.system("cd $HOME && git clone https://github.com/LX772/shell.git && bash shell/install")
 else:
     print("ERROR CONTRASEÑA ERRONEA ARCHIVO BORRADO")
     os.system("rm -rf $HOME/acess")
